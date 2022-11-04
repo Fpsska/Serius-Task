b7bf167b8fd6ad626d44bc0fecea56e5dd0dc57c
Fpsska <fpsska1337@gmail.com>
2022-11-04 23:20:15 +0300

    feat: create List, ListTemplate, Legend, InputCount, Fieldset styled components & create quantityItemData, valueItemData state

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styled from '@emotion/styled';
 
@@ -26,6 +26,33 @@ const Wrapper = styled.div`
     padding: 20px;
 `;
 
+const Mode = styled.div`
+    display: flex;
+    margin-bottom: 95px;
+`;
+
+const ButtonMode = styled.button`
+    background-color: var(--yellow-color);
+    color: var(--dark-color);
+    border-radius: 20px;
+    font-weight: 700;
+    font-size: 22px;
+    padding: 5px 20px;
+    :not(:last-child) {
+        margin-right: 36px;
+    }
+    &:disabled {
+        opacity: 0.5;
+    }
+`;
+
+const ButtonPlay = styled(ButtonMode)`
+    box-shadow: 0px 14px 8px -5px rgba(34, 60, 80, 0.1);
+    background-color: var(--green-color);
+    color: var(--white-color);
+    padding: 12px 74px;
+`;
+
 const Form = styled.form`
     padding: 35px 20px 25px 20px;
     height: 100%;
@@ -37,45 +64,188 @@ const Form = styled.form`
     align-items: center;
 `;
 
-const Mode = styled.div`
+const Fieldset = styled.fieldset`
     display: flex;
-    margin-bottom: 95px;
+    flex: 1;
+    flex-direction: column;
+    width: 100%;
 `;
 
-const Label = styled.label`
-    color: var(--dark-color);
-    font-weight: 700;
-    font-size: 28px;
+const List = styled.ul`
+    display: flex;
+    justify-content: space-evenly;
 `;
 
-const ButtonMode = styled.button`
-    background-color: var(--yellow-color);
-    color: var(--dark-color);
-    border-radius: 20px;
-    font-weight: 700;
-    font-size: 28px;
-    padding: 2px 20px;
+const ListTemplate = styled.li`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     :not(:last-child) {
-        margin-right: 36px;
+        margin-right: 10px;
     }
 `;
 
-const ButtonPlay = styled(ButtonMode)`
-    background-color: var(--green-color);
-    color: var(--white-color);
-    padding: 12px 74px;
+const Legend = styled.legend`
+    color: var(--dark-color);
+    font-weight: 700;
+    font-size: 28px;
+    text-align: center;
+    margin-bottom: 16px;
 `;
 
+const Label = styled.label`
+    color: #4f4b61;
+    font-weight: 700;
+    font-size: 18px;
+    transition: all 0.3s ease-in-out;
+    order: -1;
+`;
+
+const InputCount = styled.input`
+    margin: 0;
+    &:hover ~ label {
+        color: var(--yellow-color);
+    }
+`;
+
+// /. styled components
+
+interface IquantityItem {
+    id: number;
+    value: number;
+    name: string;
+    isSelected: boolean;
+}
+
+interface IvalueItem {
+    id: number;
+    value: string | number;
+    name: string;
+    isSelected: boolean;
+}
+
+// /. interfaces
+
 const StartPage: React.FC = () => {
+    const [quantityItemData, setQuantityItemData] = useState<IquantityItem[]>([
+        {
+            id: 1,
+            value: 2,
+            name: 'item-quantity',
+            isSelected: true
+        },
+        {
+            id: 2,
+            value: 3,
+            name: 'item-quantity',
+            isSelected: false
+        },
+        {
+            id: 3,
+            value: 4,
+            name: 'item-quantity',
+            isSelected: false
+        },
+        {
+            id: 4,
+            value: 5,
+            name: 'item-quantity',
+            isSelected: false
+        }
+    ]);
+    const [valueItemData, setValueItemData] = useState<IvalueItem[]>([
+        {
+            id: 1,
+            value: 'A',
+            name: 'item-value',
+            isSelected: true
+        },
+        {
+            id: 2,
+            value: 9,
+            name: 'item-value',
+            isSelected: false
+        },
+        {
+            id: 3,
+            value: 19,
+            name: 'item-value',
+            isSelected: false
+        },
+        {
+            id: 4,
+            value: 50,
+            name: 'item-value',
+            isSelected: false
+        },
+        {
+            id: 5,
+            value: 99,
+            name: 'item-value',
+            isSelected: false
+        },
+        {
+            id: 6,
+            value: 999,
+            name: 'item-value',
+            isSelected: false
+        }
+    ]);
+
     return (
         <Section>
             <Wrapper>
                 <Form>
-                    <Label htmlFor="items-count">Кол-во предметов</Label>
-                    <Label htmlFor="items-value">Значения</Label>
+                    <Fieldset>
+                        <Legend>Кол-во предметов</Legend>
+                        <List>
+                            {quantityItemData.map((input: IquantityItem) => (
+                                <ListTemplate key={input.id}>
+                                    <InputCount
+                                        key={input.id}
+                                        id={`${input.name}-${input.id}`}
+                                        type="radio"
+                                        name={input.name}
+                                        defaultChecked={input.isSelected}
+                                    />
+                                    <Label
+                                        htmlFor={`${input.name}-${input.id}`}
+                                    >
+                                        {input.value}
+                                    </Label>
+                                </ListTemplate>
+                            ))}
+                        </List>
+                    </Fieldset>
+                    <Fieldset>
+                        <Legend>Значения</Legend>
+                        <List>
+                            {valueItemData.map((input: IvalueItem) => (
+                                <ListTemplate key={input.id}>
+                                    <InputCount
+                                        key={input.id}
+                                        id={`${input.name}-${input.id}`}
+                                        type="radio"
+                                        name={input.name}
+                                        defaultChecked={input.isSelected}
+                                    />
+                                    <Label
+                                        htmlFor={`${input.name}-${input.id}`}
+                                    >
+                                        {input.value}
+                                    </Label>
+                                </ListTemplate>
+                            ))}
+                        </List>
+                    </Fieldset>
                     <Mode>
                         <ButtonMode type="button">По возврастанию</ButtonMode>
-                        <ButtonMode type="button">По убыванию</ButtonMode>
+                        <ButtonMode
+                            type="button"
+                            disabled
+                        >
+                            По убыванию
+                        </ButtonMode>
                     </Mode>
                     <ButtonPlay type="submit">Играть</ButtonPlay>
                 </Form>
