25af5e67ea73e69e72df9e0ffaa81997e74137f3
Fpsska <fpsska1337@gmail.com>
2022-11-19 20:58:24 +0300

    feat: add logic of set start/initial value for generate numbersOfRange[] in getRandomNumOfRange helper func

--- src/helpers/getRandomNumOfRange.ts
+++ src/helpers/getRandomNumOfRange.ts
@@ -2,17 +2,38 @@ export function getRandomNumOfRange(
     range: number,
     outputCount: number
 ): number[] {
-    const arr = [];
-    for (let i = 0; i <= range; i++) {
-        arr.push(i);
+    let minVal = 0;
+
+    // handle start/initial value for generated [] including diapason
+    if (range >= 1 && range <= 9) {
+        minVal = 1;
+    }
+    if (range >= 10 && range <= 19) {
+        minVal = 10;
+    }
+    if (range >= 20 && range <= 50) {
+        minVal = 20;
+    }
+    if (range >= 51 && range <= 99) {
+        minVal = 51;
+    }
+    if (range >= 100 && range <= 999) {
+        minVal = 100;
+    }
+
+    const numbersOfRange = [];
+    for (let i = minVal; i <= range; i++) {
+        // generate including diapason
+        numbersOfRange.push(i);
     }
 
     const result = [];
 
     for (let i = 0; i < outputCount; i++) {
-        const random = Math.floor(Math.random() * (range - i));
-        result.push(arr[random]);
-        arr[random] = arr[range - i];
+        // generate including diapason, limit items in out []
+        const randomIDX = Math.floor(Math.random() * numbersOfRange.length);
+        result.push(numbersOfRange[randomIDX]);
+        numbersOfRange[randomIDX] = numbersOfRange[range - i];
     }
 
     return result;
